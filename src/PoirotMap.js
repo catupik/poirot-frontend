b6dc1e273f17f32c03284e95a7543184fe53d165
Katerina Gnedysheva <56027302+catupik@users.noreply.github.com>
2024-01-23 00:40:55 +0100

    Update PoirotMap.js
    api key

--- src/PoirotMap.js
+++ src/PoirotMap.js
@@ -236,7 +236,7 @@ const PoirotMap = () => {
         style={{ height: "400px", width: "100%" }}
       >
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?key=6839e759-b69d-4cf6-b5ff-17dfdd1e4173"
           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         />
 
