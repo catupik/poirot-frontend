36eb51e0ecaaf86538f485dcf9184337de7cdd02
catupik <gnedyshevaeg@gmail.com>
2024-01-27 15:47:59 +0100

    modal

--- src/Stripe/CheckoutForm.js
+++ src/Stripe/CheckoutForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import axios from "axios";
 import { getTotalPrice, clearCart } from "../redux/cartSlice";
