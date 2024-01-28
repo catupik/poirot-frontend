3affa528b8b00069db8d96971f8c6e0d6c6fbd76
catupik <gnedyshevaeg@gmail.com>
2024-01-28 16:04:52 +0100

    footer text

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,7 @@ const Footer = () => {
     return(
         <div className="footer">
             <p>
-            This website was developed solely for educational purposes. All rights reserved by the author, not the developer. (gnedyshevaeg@gmail.com)
-            </p>
+            This website has been created exclusively for educational purposes. All rights are reserved by the author. The developer does not hold any rights to the content. For inquiries, contact gnedyshevaeg@gmail.com            </p>
             
         </div>
     )
