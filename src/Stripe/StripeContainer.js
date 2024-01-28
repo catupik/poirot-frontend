2acd2cba0244fc5e01bd24d4c2196b363aa22370
catupik <gnedyshevaeg@gmail.com>
2024-01-28 15:53:29 +0100

    stripe language

--- src/Stripe/StripeContainer.js
+++ src/Stripe/StripeContainer.js
@@ -5,7 +5,9 @@ import { Elements } from "@stripe/react-stripe-js";
 import  CheckoutForm  from "./CheckoutForm";
 
 const PUBLIC_KEY = "pk_test_51OcsVgBa3Kheo6rmbWSNKHQ2nSUxR0ovwJIqXT4RWEFDSP84eZA9kJu554secaMIR4omZv2Gs3xuAr37j7xLnKNJ00HJyhJesb";
-const stripeTestPromice = loadStripe(PUBLIC_KEY);
+const stripeTestPromice = loadStripe(PUBLIC_KEY, {
+    locale: 'en'
+});
 
 const Stripe = () => {
     return(
