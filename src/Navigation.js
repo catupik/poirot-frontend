ee124cb58736e53884e055fef6424af83b134642
catupik <gnedyshevaeg@gmail.com>
2024-01-31 22:40:45 +0100

    animation navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -8,7 +8,44 @@ import music from './poirot.mp3'
 const Navigation = ({ isMenuOpen, toggleMenu, isAuthenticated, loginWithRedirect, handleLogOut, togglePlay, isPlaying, audioRef }) => {
     
     const menuRef = useRef();
+    const titleRef = useRef();
+    const mottoRef = useRef();
+    const musicButtonRef = useRef();
+    const iconBadgeRef = useRef();
+    const burgerImg = useRef();
    
+    useEffect(() => {
+       
+        gsap.fromTo(titleRef.current, 
+            { y: -100, opacity: 0 }, 
+            { y: 0, opacity: 1, duration: 1.2, ease: "power3.out" }
+        );
+
+        
+        gsap.fromTo(mottoRef.current, 
+            { y: -100, opacity: 0 }, 
+            { y: 0, opacity: 1, duration: 1.2, ease: "power3.out" }
+        );
+
+      
+        gsap.fromTo(musicButtonRef.current, 
+            { y: -100, opacity: 0 }, 
+            { y: 0, opacity: 1, duration: 1.2, ease: "power3.out" }
+        );
+
+        
+        gsap.fromTo(iconBadgeRef.current, 
+            { y: -100, opacity: 0 }, 
+            { y: 0, opacity: 1, duration: 1.6, ease: "power3.out" }
+        );
+
+        gsap.fromTo(burgerImg.current, 
+            { y: -100, opacity: 0 }, 
+            { y: 0, opacity: 1, duration: 1.6, ease: "power3.out" }
+        );
+    }, []);
+
+
     useEffect(() => {
         if(isMenuOpen) {
             gsap.fromTo(menuRef.current, 
@@ -31,19 +68,19 @@ const Navigation = ({ isMenuOpen, toggleMenu, isAuthenticated, loginWithRedirect
     
     return (
         <nav className="navburger">
-            <div>
+            <div className="animation" >
                 <div>
-                  <p className="poirot-title">POIROT</p>
-                  <h6 className="motto">Method. Order. And little grey cells</h6> 
+                  <p className="poirot-title" ref={titleRef}>POIROT</p>
+                  <h6 className="motto"ref={mottoRef}>Method. Order. And little grey cells</h6> 
                 </div>
 
-                <button className="music" onClick={togglePlay}>
-                    {isPlaying ? <img className="music-btn" src="/account/play.png" alt="play" width='30'/> : <img className="music-btn" src="/account/noplay.png" alt="pause" width='30'/>  }
+                <button className="music" onClick={togglePlay} >
+                    {isPlaying ? <img ref={musicButtonRef} className="music-btn" src="/account/play.png" alt="play" width='30'/> : <img ref={musicButtonRef} className="music-btn" src="/account/noplay.png" alt="pause" width='30'/>  }
                  </button>
-                 <audio ref={audioRef} src={music} loop/>
-                <div className="icon-with-badge">
-                    <img onClick={toggleMenu} className="burger-icon" src="/poirot/detective.png" alt="Menu Icon"/>
-                    {totalItems > 0 && <span className="badge">{totalItems}</span>}
+                 <audio  src={music} loop/>
+                <div className="icon-with-badge" >
+                    <img ref={burgerImg}  onClick={toggleMenu} className="burger-icon" src="/poirot/detective.png" alt="Menu Icon" />
+                    {totalItems > 0 && <span ref={iconBadgeRef} className="badge">{totalItems}</span>}
                     </div>
             </div>
             
