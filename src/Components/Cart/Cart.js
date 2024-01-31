2600224695d341fa6f6bec9477198816ac25f496
catupik <gnedyshevaeg@gmail.com>
2024-01-31 12:32:32 +0100

    animation cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,27 +2,75 @@ import CartItem from "./CartItem";
 import { getCartItems, getTotalPrice } from "../../redux/cartSlice";
 import { useSelector } from "react-redux";
 import Stripe from "../../Stripe/StripeContainer";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 
 const Cart = () => {
   
   const cartItems = useSelector(getCartItems);
   const totalPrice = useSelector(getTotalPrice);
   
+  const cartTitleRef = useRef(null);
+  const cartItemsAndTotalRef = useRef([]);
+  cartItemsAndTotalRef.current = [];
+
+  const addToRefs = (el) => {
+    if (el && !cartItemsAndTotalRef.current.includes(el)) {
+      cartItemsAndTotalRef.current.push(el);
+    }
+  };
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+   
+    if (cartTitleRef.current) {
+      gsap.fromTo(
+        cartTitleRef.current,
+        { opacity: 0 },
+        { opacity: 1, duration: 2 }
+      );
+    }
+
+   
+    cartItemsAndTotalRef.current.forEach((el) => {
+      gsap.fromTo(
+        el,
+        { opacity: 0, y: 30 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 2,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: el,
+            start: "top 80%",
+            toggleActions: "play none none none",
+          },
+        }
+      );
+    });
+  }, []);
+
 
   return (
     <div className="cart-container">
       {cartItems.length === 0 ? (
-        <h2 className="cart-title">Your cart is empty</h2>
+        <h2 className="cart-title" ref={cartTitleRef}>Your cart is empty</h2>
       ) : (
         <div>
-          <div className="sticky">
+          <div className="sticky" ref={addToRefs}>
            <h2 className="cart-title">TOTAL: £{totalPrice}</h2>
            {/* <button className="btn">Check out</button> */}
            <Stripe/>
           </div>
        
          {cartItems.map((cartItem) => (
-            <CartItem key={cartItem.itemId} cartItem={cartItem} />
+            <div ref={addToRefs} key={cartItem.itemId}>
+            <CartItem cartItem={cartItem} />
+          </div>
           ))}
         </div>
       )}
--- src/Components/Items/AllItems.js
+++ src/Components/Items/AllItems.js
@@ -36,7 +36,7 @@ const AllItems = () => {
         { opacity: 0, y: 30 },
         {
           opacity: 1,
-          y: 0,
+          
           duration: 2,
           ease: "power2.out",
           scrollTrigger: {
